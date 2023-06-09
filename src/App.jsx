bca070d241583bc8272e77fe4019ec06e24b2c8f
mdrahathossenantor <md.rahathossenantor@gmail.com>
2023-06-10 01:48:43 +0600

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,12 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1>React-World</h1>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div> */}
     </>
   )
 }
